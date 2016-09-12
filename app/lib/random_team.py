a369aa3902aa2cc44fd77fc13da2a6eb7d3266ba
Gary Ditsch <garyditsch@gmail.com>
2016-09-12 16:18:51 -0400

    final working random generator app

---
+++ app/lib/random_team.py
@@ -0,0 +1,100 @@
+import random
+import os
+import copy
+import json
+from sys import argv
+
+class RandomGroups:
+    def __init__(self, number_per_group=2):
+        self.number_per_group = number_per_group
+        self.adjectives = []
+        self.nouns = []
+        self.groups = []
+        self.students = []
+        self.run_setup_functions()
+
+    def run_setup_functions(self):
+        """ Functions to be run during initilization """
+        self.set_adjectives()
+        self.set_nouns()
+        self.set_students()
+        random.shuffle(self.students)
+        self.student_list_copy = copy.deepcopy(self.students)
+        self.set_group()
+
+    def set_adjectives(self):
+        with open('app/data/adjectives.txt') as inputAdjectives:
+            self.adjectives = inputAdjectives
+            """ Strip Off Newlines"""
+            self.adjectives = [line.rstrip('\n').split(',') for line in self.adjectives]
+            """ Flatten List """
+            self.adjectives = [item for sublist in self.adjectives for item in sublist ]
+
+    def set_nouns(self):
+        with open('app/data/nouns.txt') as inputNouns:
+            self.nouns = inputNouns
+            """ Strip Off Newlines"""
+            self.nouns = [line.rstrip('\n').split(',') for line in self.nouns]
+            """ Flatten List """
+            self.nouns = [item for sublist in self.nouns for item in sublist ]
+
+    def set_students(self):
+        with open('app/data/students.json', 'r') as f:
+            self.students = json.load(f)
+
+    def set_group(self):
+        """ Main function to gather the groups """
+        while len(self.student_list_copy) > 0:
+            if len(self.student_list_copy) >= self.number_per_group:
+                choices = self.random_add_to_group()
+            elif len(self.student_list_copy) is 1:
+                choices = self.add_one_to_group()
+            else:
+                choices = self.add_remaining_to_groups()
+            self.remove(choices)
+        self.add_teams_to_groups()
+
+    def add_remaining_to_groups(self):
+        """ Add leftover students to groups if there are
+        more than one remaining students but not enough
+        for a full group """
+        choices = copy.deepcopy(self.student_list_copy)
+        self.groups.append(choices)
+        return choices
+
+    def random_add_to_group(self):
+        """ Randomly add students to group based on the 
+        passed in number per group"""
+        choices = random.sample(self.student_list_copy, self.number_per_group)
+        self.groups.append(choices)
+        return choices
+
+    def add_one_to_group(self):
+        """ Add leftover student to an existing group at random"""
+        choices = [self.student_list_copy[0]]
+        smallest_group_index = self.groups.index(min(self.groups, key=len))
+        self.groups[smallest_group_index].append(self.student_list_copy[0])
+        return choices
+
+    def remove(self, choices):
+        """ Remove selected students from the selection list """
+        for choice in choices:
+            self.student_list_copy.remove(choice)
+            
+    def add_teams_to_groups(self):
+        self.teamed_groups = []
+        for group in self.groups:
+            adj = random.choice(self.adjectives).capitalize()
+            noun = random.choice(self.nouns).capitalize()
+            str_group = ", ".join(group)
+            teams = "Team {0} {1}: {2}".format(adj, noun, str_group)
+            self.teamed_groups.append(teams)
+
+
+    def print_groups(self):
+        """ Print the groups to the terminal """
+        os.system('clear')
+        print("\n" * 8)
+        for group in self.teamed_groups:
+            print(group)
+            print(len(group) * '-' + "\n")
--- app/views.py
+++ app/views.py
@@ -2,9 +2,13 @@ from os import path
 from flask import render_template
 
 from app import app
+from app.lib.random_team import RandomGroups
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    random_groups = RandomGroups()
+
+    return render_template("index.html", random_groups=random_groups)
     
